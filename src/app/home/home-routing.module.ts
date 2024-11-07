cfaebb0ae3dbe59dc8f688fc999a11922421e1e6
valizsecs <benjamin.bocca11@gmail.com>
2024-11-07 09:39:45 -0300

    Update home-routing.module.ts

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-import { HorarioPage } from './horario.page';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: HorarioPage
+    component: HomePage,
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class HorarioPageRoutingModule {}
-
+export class HomePageRoutingModule {}
