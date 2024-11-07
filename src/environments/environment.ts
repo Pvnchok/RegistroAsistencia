a7a7307896a2aad633fe8ef0aee85eb478683c01
valizsecs <benjamin.bocca11@gmail.com>
2024-11-07 08:55:56 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   API_KEY: '6ad1185c0c216083aa4c9984ec75658e',
   API_URL: 'http://api.weatherstack.com/current'
 };
+
 
 
 /*
