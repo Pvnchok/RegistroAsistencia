8e86029a03886a44b420ed1b1a4fd83e3bf8e7c9
valizsecs <benjamin.bocca11@gmail.com>
2024-11-21 01:21:21 -0300

    Foto perfil

--- src/app/user/user.page.ts
+++ src/app/user/user.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -10,8 +11,26 @@ export class UserPage {
   userName: string = 'Panchito';
   userEmail: string = 'Panchito.tilin@duocuc.cl';
   userAge: number = 18;
-  userImage: string ="assets/img/gato.jpg";
- 
-  constructor() {}
-}
+  userImage: string = '';
 
+  constructor(private router: Router) {
+    const savedImage = localStorage.getItem('userImage');
+    this.userImage = savedImage || 'assets/img/gato.jpg';
+  }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.userImage = reader.result as string;
+        localStorage.setItem('userImage', this.userImage);
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  navigateToHome() {
+    this.router.navigate(['/home']); // Cambia '/home' al path real de tu página de inicio
+  }
+}
