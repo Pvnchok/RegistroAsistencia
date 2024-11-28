6b3810ef7a94abd272b7688496af2147db16a5c4
viikyyyy <133674067+viikyyyy@users.noreply.github.com>
2024-11-28 10:17:53 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  API_KEY: '6ad1185c0c216083aa4c9984ec75658e',
-  API_URL: 'http://api.weatherstack.com/current'
+  production: false
 };
 
 
